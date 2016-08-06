4365294af94a63e34d258bbca0409546d6c0352e
Zm4inX <ZmainX@hotmail.fr>
2016-08-06 20:08:23 +0200

    Add files via upload

---
+++ rush00/colle01.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colle01.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vjauze <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/06 16:09:51 by vjauze            #+#    #+#             */
+/*   Updated: 2016/08/06 16:09:54 by vjauze           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	draw_line(char begin, char mid, char end, int siz)
+{
+	ft_putchar(begin);
+	while ((siz - 1) > 1)
+	{
+		ft_putchar(mid);
+		siz--;
+	}
+	if (siz > 1)
+	{
+		ft_putchar(end);
+	}
+	ft_putchar('\n');
+}
+
+void	colle(int x, int y)
+{
+	if (x > 0 && y > 0)
+	{
+		draw_line('/', '*', 92, x);
+		while ((y - 1) > 1)
+		{
+			draw_line('*', ' ', '*', x);
+			y--;
+		}
+		if (y > 1)
+			draw_line(92, '*', '/', x);
+	}
+}
---
+++ rush00/colle02.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colle02.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vjauze <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/06 16:11:07 by vjauze            #+#    #+#             */
+/*   Updated: 2016/08/06 16:11:10 by vjauze           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	draw_line(char begin, char mid, char end, int siz)
+{
+	ft_putchar(begin);
+	while ((siz - 1) > 1)
+	{
+		ft_putchar(mid);
+		siz--;
+	}
+	if (siz > 1)
+	{
+		ft_putchar(end);
+	}
+	ft_putchar('\n');
+}
+
+void	colle(int x, int y)
+{
+	if (x > 0 && y > 0)
+	{
+		draw_line('A', 'B', 'A', x);
+		while ((y - 1) > 1)
+		{
+			draw_line('B', ' ', 'B', x);
+			y--;
+		}
+		if (y > 1)
+			draw_line('C', 'B', 'C', x);
+	}
+}
